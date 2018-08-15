6742d4d9835c1914be7161a48372a6259f285edb
Kiel Dehnert <kielzor@gmail.com>
2018-08-14 21:11:01 -0600

    Add styling to cards

--- src/Card.js
+++ src/Card.js
@@ -2,14 +2,26 @@ import React from 'react'
 
 
 const Card = ({data}) => {
-  const stats = Object.entries(data.stats).map(key => (
-    <p>{key[0]}: {key[1]}</p>
-))
+  const stats = Object.entries(data.stats).map(key => {
+    if (key[1] > .5) {
+      return  <div className='higher'>
+                <p className='year'>{key[0]}</p>
+                <p className='hide'>----</p>
+                <p className='val'>{key[1]}</p>
+              </div>
+    } else {
+      return  <div className='lower'>
+                <p className='year'>{key[0]}</p>
+                <p className='val'>{key[1]}</p>
+                <p className='hide'>----</p>
+              </div>
+    }
+  })
 
   return (
     <div className='card'>
-      <p className='location'>{data.location}</p>
-      <div>{stats}</div>
+      <h5 className='location'>{data.location}</h5>
+      <div className='stats'>{stats}</div>
     </div>
   )
 }
